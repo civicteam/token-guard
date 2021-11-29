b77ca6bcb6dcbaa5271374cc1977b3a2ef4b3c4f
dankelleher <dankelleher@yahoo.com>
2021-11-29 06:11:27 +0100

    IDCOM-1359 Add debug lines to help debug a flaky test on CI

--- programs/dummy-spender/src/utils.rs
+++ programs/dummy-spender/src/utils.rs
@@ -1,50 +1,54 @@
 use crate::ErrorCode;
 use anchor_lang::{
-    prelude::{AccountInfo, ProgramResult},
-    solana_program::{
-        program::invoke_signed,
-    },
+  prelude::{AccountInfo, ProgramResult, msg},
+  solana_program::{
+    program::invoke_signed,
+  },
 };
 
 /// Parameters for an SPL Token transfer CPI
 pub struct TokenTransferParams<'a: 'b, 'b> {
-    /// the source token account
-    pub source: AccountInfo<'a>,
-    /// the destination token account
-    pub destination: AccountInfo<'a>,
-    /// the amount of tokens to transfer
-    pub amount: u64,
-    /// the owner of the source account
-    pub authority: AccountInfo<'a>,
-    /// if the source authority is a PDA, the signer seeds for the account
-    pub authority_signer_seeds: &'b [&'b [u8]],
-    /// the SPL Token program
-    pub token_program: AccountInfo<'a>,
+  /// the source token account
+  pub source: AccountInfo<'a>,
+  /// the destination token account
+  pub destination: AccountInfo<'a>,
+  /// the amount of tokens to transfer
+  pub amount: u64,
+  /// the owner of the source account
+  pub authority: AccountInfo<'a>,
+  /// if the source authority is a PDA, the signer seeds for the account
+  pub authority_signer_seeds: &'b [&'b [u8]],
+  /// the SPL Token program
+  pub token_program: AccountInfo<'a>,
 }
 
 #[inline(always)]
 pub fn spl_token_transfer(params: TokenTransferParams<'_, '_>) -> ProgramResult {
-    let TokenTransferParams {
-        source,
-        destination,
-        authority,
-        token_program,
-        amount,
-        authority_signer_seeds,
-    } = params;
+  let TokenTransferParams {
+    source,
+    destination,
+    authority,
+    token_program,
+    amount,
+    authority_signer_seeds,
+  } = params;
 
-    let result = invoke_signed(
-        &spl_token::instruction::transfer(
-            token_program.key,
-            source.key,
-            destination.key,
-            authority.key,
-            &[],
-            amount,
-        )?,
-        &[source, destination, authority, token_program],
-        &[authority_signer_seeds],
-    );
+  msg!("Creating transfer instruction");
+  let transfer_instruction = spl_token::instruction::transfer(
+    token_program.key,
+    source.key,
+    destination.key,
+    authority.key,
+    &[],
+    amount,
+  )?;
 
-    result.map_err(|_| ErrorCode::TokenTransferFailed.into())
+  msg!("Sending transfer instruction");
+  let result = invoke_signed(
+    &transfer_instruction,
+    &[source, destination, authority, token_program],
+    &[authority_signer_seeds],
+  );
+
+  result.map_err(|_| ErrorCode::TokenTransferFailed.into())
 }
